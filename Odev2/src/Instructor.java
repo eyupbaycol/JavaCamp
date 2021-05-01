14ab67a4d1bd39f8dc56923c478342ffbf549b00
eyupbaycol <eyup.baycol@detaysoft.com>
2021-05-01 16:20:00 +0300

    first commit

---
+++ Odev2/src/Instructor.java
@@ -0,0 +1,21 @@
+public class Instructor extends  User{
+    private String workPlaceName;
+    private String certificates;
+
+    public String getWorkPlaceName() {
+        return workPlaceName;
+    }
+
+    public void setWorkPlaceName(String workPlaceName) {
+        this.workPlaceName = workPlaceName;
+    }
+
+    public String getCertificates() {
+        return certificates;
+    }
+
+    public void setCertificates(String certificates) {
+        this.certificates = certificates;
+    }
+
+}
---
+++ Odev2/src/ProfileUI.java
@@ -0,0 +1,5 @@
+public class ProfileUI {
+    public  void getProfileInformation(UserManager userManager, User user) {
+        userManager.getUserProfile(user);
+    }
+}
